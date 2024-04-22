d96382f26081927a0a639e5b48e4c1e88c1478ea
gamingtechedu <gamingtechedu@gmail.com>
2024-04-22 09:38:34 -0300

    commit 2

---
+++ lib/export.dart
@@ -0,0 +1,19 @@
+library ide_modular;
+
+export 'package:badges/badges.dart';
+export 'src/side_menu.dart';
+export 'src/item/export.dart';
+export 'src/side_menu_mode.dart';
+export 'src/side_menu_priority.dart';
+export 'src/side_menu_position.dart';
+export 'src/side_menu_controller.dart';
+export 'src/data/side_menu_data.dart';
+export 'src/data/side_menu_item_data.dart';
+export 'src/data/resizer_toggle_data.dart';
+export 'src/data/resizer_data.dart';
+export 'src/item/side_menu_item_admin.dart';
+export 'src/item/side_menu_item_tile_drag.dart';
+
+
+
+
